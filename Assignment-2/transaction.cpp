d1ec23b319d36b4e5ce65de4a85e41de7e9669ae
sweoo <swezinoo410@gmail.com>
2024-02-07 22:38:04 +0800

    Add transaction.cpp file

---
+++ Assignment-2/transaction.cpp
@@ -0,0 +1,32 @@
+#include "transaction.h"
+#include <string>
+
+using namespace std;
+
+Transaction_type Transaction::get_transaction_type(){
+     return transaction_type;
+}
+double Transaction::get_amount(){
+     return amount;
+}
+double Transaction::get_balance(){
+    return balance;
+}
+Date Transaction::get_date(){
+     return date;
+}
+string Transaction::to_string()  {
+        string transType;
+        if(transaction_type == DEP){
+            transType = "DEP";
+        }else if(transaction_type == INT_CR){
+             transType = "INT CR";
+        }else if(transaction_type == WD){
+             transType = "WD";
+        }else if(transaction_type == CHKCHG){
+             transType = "CHKCHG";
+        }else if(transaction_type == OD_PEN){
+             transType = "OD_PEN";
+        }
+            return  transType + "  " + date.to_string() + "  $  " + std::to_string(amount) +  "  $  " + std::to_string(balance);
+}
