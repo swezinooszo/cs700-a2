c895069be0f54e0e68b6c6af0c34d0a15f48f780
sweoo <swezinoo410@gmail.com>
2024-02-08 18:20:38 +0800

    Add internal documentation for senior.h file

--- Assignment-2/senior.h
+++ Assignment-2/senior.h
@@ -1,13 +1,28 @@
+/** @file senior.h
+*   @author Swe Zin Oo
+*   @date 8 Feb 2024
+*/
+
 #ifndef SENIOR_H
 #define SENIOR_H
 #include "customer.h"
 
+
+/** Senior is a class which extends the Customer class. In another words, it's a child class of Customer parent class. 
+ * Senior class will inherit all member varialbes and functions of the Customer class when creating an object. The Senior class includes
+ * fixed rate for saving interest, checking interest, checking charge and overdraft penaly for Senior customer.
+*/
 class Senior: public Customer{
     public:
-    const double SAVINGS_INTEREST = 0.04;		// annual rate
-    const double CHECKING_INTEREST = 0.01;      // annual rate. Yes! this is interest rate for the checking account.
-    const double CHECK_CHARGE = 0.01;           //cents for withdrawal
-    const double OVERDRAFT_PENALTY = 25.0;      //dollar
+     // Constants
+    /** annual saving account interest rate */
+    const double SAVINGS_INTEREST = 0.04;	
+    /** annual checking account interest rate */	
+    const double CHECKING_INTEREST = 0.01;    
+    /** check charge for withdrawl in cents*/
+    const double CHECK_CHARGE = 0.01;  
+    /** overdraf penalty in dollar */         
+    const double OVERDRAFT_PENALTY = 25.0; 
 };
 
 #endif
