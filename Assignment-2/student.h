dbb1cdb18946a5e0a3a4f6a54d43c0e5c8fb8671
sweoo <swezinoo410@gmail.com>
2024-02-08 18:20:56 +0800

    Add internal documentation for student.h file

--- Assignment-2/student.h
+++ Assignment-2/student.h
@@ -1,13 +1,28 @@
+/** @file student.h
+*   @author Swe Zin Oo
+*   @date 8 Feb 2024
+*/
+
 #ifndef STUDENT_H
 #define STUDENT_H
 #include "customer.h"
 
+/** Student is a class which extends the Customer class. In another words, it's a child class of Customer parent class. 
+ * Student class will inherit all member varialbes and functions of the Customer class when creating an object. The Student class includes
+ * fixed rate for saving interest, checking interest, checking charge and overdraft penaly for Student customer.
+*/
+
 class Student: public Customer{
     public:
-    const double SAVINGS_INTEREST = 0.04; 		// annual rate
-    const double CHECKING_INTEREST = 0.01;		// annual rate. Yes! this is interest rate for the checking account.  
-    const double CHECK_CHARGE = 0.02; 			//cents for withdrawal
-    const double OVERDRAFT_PENALTY = 25.0; 	//dollar 
+     // Constants
+    /** annual saving account interest rate */
+    const double SAVINGS_INTEREST = 0.04;
+    /** annual checking account interest rate */	
+    const double CHECKING_INTEREST = 0.01;		
+    /** check charge for withdrawl in cents*/
+    const double CHECK_CHARGE = 0.02; 	
+    /** overdraf penalty in dollar */  		
+    const double OVERDRAFT_PENALTY = 25.0;
 };
 
 #endif
