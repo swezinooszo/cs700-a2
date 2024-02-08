45551a57d54320ae2a5eb4eb083caec0c9226dcf
sweoo <swezinoo410@gmail.com>
2024-02-08 18:21:33 +0800

    Add internal documentation for transaction.h file

--- Assignment-2/transaction.h
+++ Assignment-2/transaction.h
@@ -1,3 +1,7 @@
+/** @file Transaction.h
+*   @author Swe Zin Oo
+*   @date 8 Feb 2024
+*/
 #ifndef TRANSACTION_H
 #define TRANSACTION_H
 #include "Date.h"
@@ -8,23 +12,52 @@ enum Transaction_type {
     DEP, INT_CR, WD,CHKCHG,OD_PEN
 };
 
+/** Transaction is a class which stores the all the information of a transaction */
 class Transaction{
 
     private:
+        // Data Fields
+        /** The transaction type */
         Transaction_type transaction_type;
+        /** The amount */
         double amount;
+        /** The balance */
         double balance;
+        /** The date */
         Date date;
 
     public:
+        //Constructors
+        /** Constructs a transaction with given values.
+         * @param tranType the transaction type
+         * @param amt the amount to be stored in the transaction
+         * @param bal the balance of an account to be stored in transaction
+         * @param d the transaction date
+        */
         Transaction(Transaction_type tranType, double amt, double bal, Date d)
                 : transaction_type(tranType), amount(amt), balance(bal), date(d) {
                   
                 }
-        Transaction_type get_transaction_type();        
+        // Accessor Function
+        /** Gets the transaction type
+         * @return an enum Transaction_type value. 
+        */
+        Transaction_type get_transaction_type();  
+        /** Gets the amount
+         * @return the amount of the transaction
+        */      
         double get_amount();
+         /** Gets the balance
+         * @return the balance in the account at the time of the transaction
+        */ 
         double get_balance();
+         /** Gets the date
+         * @return the transaction date
+        */ 
         Date get_date();
+         /** Gets the details of transaction as a string
+         * @return  a string value which returns all the member variables 
+        */ 
         string to_string()  ;
     
 };
