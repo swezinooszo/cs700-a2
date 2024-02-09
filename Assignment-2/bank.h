a86a34b6abc7c3da0871442e9ebf7ebe21c091d2
sweoo <swezinoo410@gmail.com>
2024-02-09 18:43:13 +0800

    update internal documentation for bank.h file

--- Assignment-2/bank.h
+++ Assignment-2/bank.h
@@ -9,7 +9,10 @@
 #include <vector>
 #include "saving_account.h"
 
-/** Banks is a class which holds the information of customers' accounts. It also have include main operations such as making deposit and withdrawl */
+/** Banks is a class which holds the information of customers and their accounts. It can be considered as a secondary entry point of the program. 
+ * The implementation details of adding accounts, retrieving accounts, making deposits, applying interest, processing withdrawals and 
+ * checking for insufficient funds in the account, are all written here.
+*/
 class Bank{
     private:
         // Data Fields.
@@ -20,13 +23,13 @@ class Bank{
         virtual ~Bank() {}
 
         // Accessor Functions
-        /** Gets the accounts
+        /** Gets the accounts from the bank
          * @return the accounts as a vector array
         */
         std::vector<Account*> get_accounts() const;
 
         // Other Functions
-         /** Gets an account based on account number
+         /** Gets an account from the bank based on the account number
          * @param accountNumber the account number
          * @return an account as a pointer object
         */
@@ -35,14 +38,17 @@ class Bank{
          * @param account an account pointer object
         */
         void add_account(Account* account);
-        /** make deposit to the bank
+        /** make deposit to the bank. Each time a customer makes a deposit, the interest amount is calculated according to the annual rate and the number of days
+         *  the account's balance has been held. Subsequently, the interest amount is added to the balance of the account.
          * @param accountNumber the account number to be deposited
          * @param amount the amount to be deposited
          * @param date the date to be deposited
          * @return the balance of account after deposited as a double value
         */
         double make_deposit(int accountNumber,double amount,string date);
-        /** make withdrawl from the bank
+        /** make withdrawl from the bank.Each time a customer makes a withdrawl, the interest amount is calculated according to the annual rate and the number of days
+         *  the account's balance has been held. This interest amount is then added to the account's balance. Additionally, if the account is a checking account, 
+         *  a checking charge is calculated and deducted from the account's balance.
          * @param accountNumber the account number to be withdrawn
          * @param amount the amount to be withdrawn
          * @param date the withdrawl date
@@ -50,8 +56,10 @@ class Bank{
         */
         double make_withdrawl(int accountNumber,double amount,string date);
         /** determine whether the provided amount is withdrawable from the account. 
-         * This function is always called before customer make withdrawl.
-         * If there is no sufficient fund in the account, charge overdraft penalty to the acount.
+         * This function is always called before a customer make a withdrawl.
+         * If there are insufficient funds in the account, an overdraft penalty is charged to the account, and 
+         * the function returns a boolean value of true. If there are enough funds in the account, an overdraft penalty 
+         * is not charged, and the function returns a boolean value of false.
          * @param accountNumber the account number to be withdrawn
          * @param amount the amount to be withdrawn
          * @param date the withdrawl date
